efb0162f7630c58aee44f91ed979828a6d384aca
James Hogan <5aru@users.noreply.github.com>
2017-10-09 20:25:03 -0400

    Adding attribution for the new bash version of script

---
+++ linuxprivchecker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###################################80 Columns###################################
+#                   Linux Privilege Escalation Check Script
+#
+# [Details:] 
+## Originally forked from the linuxprivchecker.py (Mike Czumak), this script
+## is intended to be executed locally on a Linux box to enumerate basic system 
+## info and search for common privilege escalation vectors such as word 
+## writable files, misconfigurations, clear-text password and applicable 
+## exploits.
+#
+# [Original Author]: Mike Czumak (T_v3rn1x) -- @SecuritySift
+# [Contributors]:
+## Mike Merrill (linted) -- https://github.com/linted
+## James Hogan (5aru) -- https://github.com/5aru
+##################################80 Columns###################################
