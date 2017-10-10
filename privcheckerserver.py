540134ebc5371971d7663f1e15d8422af5c7dc87
linted <linted90@hotmail.com>
2017-10-10 13:54:56 -0400

    created initial server and handler

--- privcheckerserver.py
+++ privcheckerserver.py
@@ -4,15 +4,27 @@
 from exploitdb import exploitdb
 import socketserver
 
+_PORT_ = 4521
+_IP_ = '0.0.0.0'
 
-class ExploitServer(exploitdb.ExploitSearch, socketserver.StreamReqstHandler):
-    def __init__(self, ip=None, port=None):
-        super(exploitdb.ExploitSearch).__init__()
-        pass
+class SearchHandler(socketserver.StreamRequestHandler):
+    def handle():
+        self.data = self.rfile.readline().strip()
+        results = self.server.search(data)
+        output = '\n'.join([''.join(k,v) for k,v in results])
+        self.wfile.write(output)
+        #self.server <- use this is access the server
+
+
+class ExploitServer(exploitdb.ExploitSearch, socketserver.ThreadingMixIn, socketserver.TCPServer):
+    def __init__(self, connectionInfo, handler):
+        super().__init__()
+        super(exploitdb.ExploitSearch).__init__(connectionInfo, handler)
+        
 
 
 def main():
-    exploit = ExploitServer()
+    exploit = ExploitServer((_IP_, _PORT_), SearchHandler)
     
 if __name__ == "__main__":
     main()
